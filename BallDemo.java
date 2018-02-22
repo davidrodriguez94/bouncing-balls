f24556bc928d40d563c9530b46ae3c0efd89c888
David Rodriguez <rodriguezdiezdavid94@gmail.com>
2018-02-22 19:04:51 +0100

    Ahora se pueden crear tantas bolas como se indique por parametro

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,7 +11,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private BouncingBall[] myBalls;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
@@ -23,15 +23,25 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
         int ground = 400;   // position of the ground line
 
+        myBalls = new BouncingBall[numberOfBalls];
+        
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
+        //creamos tantas bolas como indiquemos
+        for(int i =0;i < numberOfBalls; i++){
+            BouncingBall ball = new BouncingBall(50+(10*i),50,10,Color.BLUE, ground,myCanvas);
+            ball.draw();
+            myBalls[i] = ball;
+        }
+        
+        
         // crate and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
@@ -42,11 +52,14 @@ public class BallDemo
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            
+            for(int i=0;i< numberOfBalls;i++){
+                myBalls[i].move();
+                
+                // stop once ball has travelled a certain distance on x axis
+                if(myBalls[i].getXPosition() >= 550){
+                    finished = true;
+                }
             }
         }
     }
